567b3739957fde8a508e1bb48111c65e9f888961
anthonyckleung <anthonyleung124@gmail.com>
2023-12-29 11:12:58 -0500

    app changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,16 @@ st.set_page_config(layout="wide")
 ar_df = pd.read_csv('data/ar_results.csv')
 
 item_df = pd.read_csv('data/popular_product.csv')
-item_df = item_df[:20]
-# ar_df.head()
+aisle_df = pd.read_csv('data/popular_aisle.csv')
+department_df = pd.read_csv('data/popular_department.csv')
 
-# Network Graph
+# Truncate data to top 20
+item_df = item_df[:20]
+aisle_df = aisle_df[:20]
+department_df= department_df[:20]
+
+
+# Load Network Graph
 HtmlFile = open(f'html/pyvis_graph.html', 'r', encoding='utf-8')
 source_code = HtmlFile.read()
 
@@ -28,34 +34,73 @@ container_3 = st.container()
 
 
 with container_1:
-  st.title(':basket: Supermarket Basket Analysis Dashboard')
+  st.header(':basket: Supermarket Basket Analysis Dashboard', divider='gray')
   col1, col2, col3 = st.columns(3)
-  col1.metric(label='No. Transactions', value=prettify(1384617), delta='4% (Month-over-Month)')
-  col2.metric(label='Avg. Basket Size per Transaction', value=31, delta='0.7% (Month-over-Month)')
+  col1.metric(label='No. Order', value=prettify(1384617), delta='4% (Month-over-Month)')
+  col2.metric(label='Avg. Basket Size per Order', value=31, delta='0.7% (Month-over-Month)')
+  st.divider()
 
 
 with container_2:
+  st_col1, st_col2, st_col3 = st.columns(3)
+
+  with st_col1:
+    st.header('Top 20 Orders by Product')
+    fig=px.bar(item_df,
+      x='purchase_count',
+      y='product_name', 
+      orientation='h',
+      labels={
+        'purchase_count': 'No. of Orders',
+        'product_name': 'Product Name'
+      },
+      height = 500,
+    )
+    fig.update_layout(yaxis=dict(autorange="reversed"))
+    st.write(fig)
+
+  with st_col2:
+    st.header('Top 20 Orders by Aisle')
+    fig=px.bar(aisle_df,
+      x='purchase_count',
+      y='aisle', 
+      orientation='h',
+      labels={
+        'purchase_count': 'No. of Orders',
+        'aisle': 'Aisle'
+      },
+      height = 500,
+    )
+    fig.update_layout(yaxis=dict(autorange="reversed"))
+    st.write(fig)
+
+  with st_col3:
+    st.header('Top 20 Orders by Department')
+    fig=px.bar(department_df,
+      x='purchase_count',
+      y='department', 
+      orientation='h',
+      labels={
+        'purchase_count': 'No. of Orders',
+        'department': 'Department'
+      },
+      height = 500,
+    )
+    fig.update_layout(yaxis=dict(autorange="reversed"))
+    st.write(fig)
+  st.divider()
+
+
+with container_3:
   
-  st_col1, st_col2 = st.columns([1,1])
+  st_col1, st_col2 = st.columns([2,1])
 
   with st_col1:
     st.header('Basket Analysis Network')
     st.components.v1.html(source_code, height=500)
   
   with st_col2:
-    st.header('Top 20 Popular Products')
-    fig=px.bar(item_df,
-      x='purchase_count',
-      y='product_name', 
-      orientation='h',
-      height = 500,
-    )
-    fig.update_layout(yaxis=dict(autorange="reversed"))
-    st.write(fig)
+    st.header('Top 200 Association Results')
+    st.dataframe(data =ar_df, height=500)
 
 
-
-
-with container_3:
-  st.header('Top 200 Association Results')
-  st.dataframe(data =ar_df, height=500)
